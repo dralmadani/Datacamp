a1a4b00ecad7735a972513478681384426bf292e
Almadani <almadaniedu@gmail.com>
2019-05-27 10:42:52 +0300

    Update and rename loc-and-iloc-2 to loc-and-iloc-2.py

--- loc-and-iloc-2.py
+++ loc-and-iloc-2.py
@@ -20,7 +20,8 @@ import pandas as pd
 cars = pd.read_csv('cars.csv', index_col = 0)
 
 # Print out drives_right value of Morocco
-print (cars.loc[['RU', 'MOR'],['country','drives_right']])
+print (cars.loc[['MOR'],['drives_right']])
 
 # Print sub-DataFrame
 print (cars.iloc[[4,5],[1,2]])
+print (cars.loc[['RU','MOR'], ['country','drives_right']])
