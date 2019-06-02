38aaf84ce24a3b487790349dbe725a4f632e0569
Almadani <almadaniedu@gmail.com>
2019-06-02 15:15:26 +0300

    Update and rename ch3-11-Driving-right-1 to ch3-11-Driving-right-1.py

--- ch3-11-Driving-right-1.py
+++ ch3-11-Driving-right-1.py
@@ -11,3 +11,16 @@ Extract the drives_right column as a Pandas Series and store it as dr.
 Use dr, a boolean Series, to subset the cars DataFrame. Store the resulting selection in sel.
 Print sel, and assert that drives_right is True for all observations.
 '''
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Extract drives_right column as Series: dr
+dr = cars["drives_right"]
+print (dr) # to build True and False
+# Use dr to subset cars: sel
+sel = cars[dr] # to get True only
+
+# Print sel
+print (sel)
+
