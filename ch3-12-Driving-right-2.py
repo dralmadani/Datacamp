9975bf2f34242fc4fa378ad21116d8aa514f11e0
Almadani <almadaniedu@gmail.com>
2019-06-06 00:06:51 +0300

    Update ch3-12-Driving-right-2.py

--- ch3-12-Driving-right-2.py
+++ ch3-12-Driving-right-2.py
@@ -11,4 +11,11 @@ import pandas as pd
 cars = pd.read_csv('cars.csv', index_col = 0)
 
 # Convert code to a one-liner
+#dr = cars['drives_right']
+#sel = cars[dr]
+
+# Print sel
+#print(sel)
+
+# Convert code to a one-liner
 print (cars [cars['drives_right']])
